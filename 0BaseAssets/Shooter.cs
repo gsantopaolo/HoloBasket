c3a94662dc61484484816bfd6fb1808f819fe911
Gian Paolo Santopaolo <gianpaolo.santopaolo@softwarelab.it>
2017-01-24 00:18:11 +0100

    initial commit chapter 1

---
+++ 0BaseAssets/Shooter.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR.WSA.Input;
+
+public class Shooter : MonoBehaviour
+{
+    public Rigidbody prefab;
+    public float speed = 10.0f;
+
+    private GestureRecognizer gestureRecognizer;
+
+    void Start()
+    {
+        //Create a new GestureRecognizer.Sign up for tapped events.
+
+      gestureRecognizer = new GestureRecognizer();
+      gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap);
+
+        gestureRecognizer.TappedEvent += GestureRecognizer_TappedEvent;
+
+        // Start looking for gestures.
+        gestureRecognizer.StartCapturingGestures();
+    }
+
+    private void GestureRecognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+    {
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        var obj = (Rigidbody)Instantiate(prefab, Camera.main.transform.position, Camera.main.transform.rotation);
+        obj.velocity = (Camera.main.transform.forward) * speed;
+    }
+
+    void OnDestroy()
+    {
+        gestureRecognizer.StopCapturingGestures();
+        gestureRecognizer.TappedEvent -= GestureRecognizer_TappedEvent;
+    }
+}
